190ec22ba2cf69348bcb0b988e4382862492276e
Farhad <farhad100@gmail.com>
2021-09-16 10:35:32 +0300

    commit on main

---
+++ Ch2.js
@@ -0,0 +1,18 @@
+/* 
+Exercise to make the following pattern.
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+var content = '';
+
+for (var i = 0; i < 7; i++) {
+    content = content + '#';
+    console.log(content);
+}
